66b9f6ff84b6d230c9e7b5d4ddf91cc3b976e6c7
Tyler Firth <tyfirth@gmail.com>
2020-06-07 21:37:51 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -8,8 +8,8 @@ class Project
     @backers = []
   end
   
-  def add_backer(name)
-    @backers << name
+  def add_backer(backer)
+    @backers << backer
     backer.back_project(self) unless backer.backed_projects.include?(self)
   end
   
